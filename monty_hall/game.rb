940ec6bd538a3ad2173ed8298f5f67a8439544c0
pfriedman <psfriedman@gmail.com>
2018-04-15 10:56:54 -0700

    Monty Hall RF ruby learning

--- monty_hall/game.rb
+++ monty_hall/game.rb
@@ -1,73 +1,73 @@
 class Game
-  
-  attr_accessor :car_door_number
-  attr_accessor :chosen_door_number
-  
-  def initialize(car_door_number = nil)
-    @car_door_number = car_door_number || rand(3)
-    @doors = []
-    3.times { |x| @doors << Door.new("goat") }
-    @doors[@car_door_number].prize = "car"
+
+  attr_accessor :doors, :chosen_door_number, :revealed_door_number
+
+  def initialize
+    @doors = prizes.map { |prize| Door.new(prize) }.shuffle
   end
-  
-  def doors
-    @doors
-  end
-  
-  def choose_door(chosen_door_number)
-    @chosen_door_number = chosen_door_number
+
+  def choose_door(n)
+    @chosen_door_number = n
     self
   end
-  
-  def opened_door_number
-    ([0,1,2] - [chosen_door_number, car_door_number]).first
-  end
-  
-  def stay_winner?
+
+  def winner?
     chosen_door_number == car_door_number
   end
-  
+
+  def show_contestant_a_goat
+    @revealed_door_number = ([0...@doors.size] - [car_door_number, chosen_door_number]).sample
+  end
+
+  def car_door_number
+    @doors.index { |door| door.prize == "car" }
+  end
+
+  private
+
+    def prizes
+      %w(car goat goat)
+    end
+
 end
 
 class Door
-  
+
   attr_accessor :prize
-  
+
   def initialize(prize)
     @prize = prize
   end
-  
 end
 
 class Simulation
-  
+
   attr_accessor :swap_win_count
   attr_accessor :stay_win_count
-  
+
   def run(number_of_games)
     @stay_win_count = 0
     @swap_win_count = 0
     number_of_games.times do
       game = Game.new
       game.choose_door(rand(3))
-      if game.stay_winner?
-        @stay_win_count += 1 
+      if game.winner?
+        @stay_win_count += 1
       else
         @swap_win_count += 1
       end
     end
   end
-  
+
   def swap_win_percentage
     (@swap_win_count.to_f / total) * 100
   end
-  
+
   def stay_win_percentage
     100 - swap_win_percentage
   end
-  
+
   def total
     @stay_win_count + @swap_win_count
   end
-  
 end
--- monty_hall/game_spec.rb
+++ monty_hall/game_spec.rb
@@ -1,87 +1,73 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "game"))
 
 describe Game do
-  
+
   let(:game) { Game.new }
-  
-  it "should have three doors" do
-    game.should have(3).doors
+
+  it "has three doors" do
+    expect(game.doors.size).to eq 3
   end
-  
-  it "should have one prize behind each door (which is a string - to be redeemed)" do
-    game.doors.each do |door| 
-      door.prize.should_not be_nil 
-      door.prize.is_a?(String).should be_true
+
+  it "has one prize behind each door (which is a string - to be redeemed)" do
+    game.doors.each do |door|
+      expect(door.prize).to_not be_nil
     end
   end
-  
-  it "should only have one car behind all three doors" do
-    car_doors = []
-    game.doors.each { |door| car_doors << door if door.prize == "car" }
-    car_doors.size.should == 1
+
+  it "only has one car behind all three doors" do
+    expect(game.doors.select do |door|
+      door.prize == "car"
+    end.size).to eq 1
   end
-  
-  it "should only have two goats behind the other doors" do
-    goat_doors = []
-    game.doors.each { |door| goat_doors << door if door.prize == "goat" }
-    goat_doors.size.should == 2
+
+  it "only has two goats behind the other doors" do
+    expect(game.doors.select do |door|
+      door.prize == "goat"
+    end.size).to eq 2
   end
-  
+
+  # will fail 1/3^19 times
   it "should not always have a car behind the same door" do
     car_doors = []
     20.times do |x|
       game = Game.new
-      game.doors.each_with_index {|door, index| car_doors << index if door.prize == "car"}
+      game.doors.each_with_index { |door, index| car_doors << index if door.prize == "car" }
     end
-    car_doors.uniq.size.should > 1
-    car_doors.uniq.size.should == 3
-  end
-  
-  it "should allow player to choose a door" do
-    game.choose_door(2).chosen_door_number.should == 2
-  end
-  
-  it "should know if the chosen door is a winner" do
-    game.choose_door(game.car_door_number).should be_stay_winner
-  end
-  
-  it "should allow the ability to set the car door 0" do
-    game = Game.new(0)
-    game.car_door_number.should == 0
-    game.doors.collect(&:prize).should == %w(car goat goat)
+    expect(car_doors.uniq.size).to eq 3
   end
 
-  it "should allow the ability to set the car door 1" do
-    game = Game.new(1)
-    game.doors.collect(&:prize).should == %w(goat car goat)
+  it "allows a player to choose a door" do
+    expect(game.choose_door(2).chosen_door_number).to eq 2
   end
-  
-  it "should allow the ability to set the car door 2" do
-    game = Game.new(2)
-    game.doors.collect(&:prize).should == %w(goat goat car)
+
+  it "knows the car door number" do
+    expect(game.car_door_number).to be_kind_of(Integer)
   end
-  
+
+  it "knows if the chosen door is a winner" do
+    expect(game.choose_door(game.car_door_number)).to be_winner
+  end
+
+  it "knows if the chosen door is not a winner" do
+    not_the_car_door_number = (game.car_door_number + 1) % 3
+    expect(game.choose_door(not_the_car_door_number)).to_not be_winner
+  end
+
   it "should pick a goat door to show the player if the player chose the car door" do
-    Game.new(1).choose_door(1).opened_door_number.should == 0
+    game.choose_door(game.car_door_number)
+    game.show_contestant_a_goat
+    expect(game.revealed_door_number).to_not eq game.car_door_number
   end
-  
-  it "should pick a goat door to show the player if the player chose the goat door 1" do
-    Game.new(0).choose_door(1).opened_door_number.should == 2
-  end
-  
-  it "should pick a goat door to show the player if the player chose the goat door 2" do
-    Game.new(0).choose_door(2).opened_door_number.should == 1
-  end
-  
+
   describe "simulation" do
     let(:simulation) { Simulation.new }
-    
+
     it "should win with a swap strategy 66% of the time and with a stay strategy 33% of the time" do
-      simulation.run(50000)
-      (65...67).should include(simulation.swap_win_percentage)
-      (32...34).should include(simulation.stay_win_percentage)
+      simulation.run(10000000)
+      expect(simulation.swap_win_percentage).to match(65...67)
+      expect(simulation.stay_win_percentage).to match(32...34)
     end
-    
+
   end
-  
+
 end
